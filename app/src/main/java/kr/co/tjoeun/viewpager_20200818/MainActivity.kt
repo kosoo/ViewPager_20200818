c783ba9ce2ed425e0b5771fa14af76f35b4492d4
이용찬 <lee9890@naver.com>
2020-08-18 20:56:04 +0900

    실제 뷰 페이징

--- app/src/main/java/kr/co/tjoeun/viewpager_20200818/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/viewpager_20200818/MainActivity.kt
@@ -2,8 +2,12 @@ package kr.co.tjoeun.viewpager_20200818
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kr.co.tjoeun.viewpager_20200818.adapters.MainViewPagerAdapter
 
 class MainActivity : BaseActivity() {
+
+    lateinit var mMainPagerAdapter : MainViewPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,9 +17,11 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
-        
+
     }
 
     override fun setValues() {
+        mMainPagerAdapter = MainViewPagerAdapter(supportFragmentManager)
+        mainViewPager.adapter = mMainPagerAdapter
     }
 }
--- app/src/main/java/kr/co/tjoeun/viewpager_20200818/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/viewpager_20200818/adapters/MainViewPagerAdapter.kt
@@ -6,15 +6,15 @@ import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import kr.co.tjoeun.viewpager_20200818.fragments.AddressFragment
 import kr.co.tjoeun.viewpager_20200818.fragments.NameFragment
-import kr.co.tjoeun.viewpager_20200818.fragments.Phon
+import kr.co.tjoeun.viewpager_20200818.fragments.PhoneFragment
 
 class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
     override fun getItem(position: Int): Fragment {
 
         if (position == 0){
-            return nameFragment()
+            return NameFragment()
         } else if (position == 1) {
-            return phoneFragment()
+            return PhoneFragment()
         } else {
             return AddressFragment()
         }
