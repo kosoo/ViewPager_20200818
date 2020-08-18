809ea1f89e0930cfcd3c0c6b7eb32bac83fd548f
이용찬 <lee9890@naver.com>
2020-08-18 21:46:40 +0900

    return when을 이용하는 예시   => 코틀린의 상황별 리턴처리 정석 코드

--- app/src/main/java/kr/co/tjoeun/viewpager_20200818/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/viewpager_20200818/adapters/MainViewPagerAdapter.kt
@@ -11,23 +11,21 @@ import kr.co.tjoeun.viewpager_20200818.fragments.PhoneFragment
 class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
 
     override fun getPageTitle(position: Int): CharSequence? {
-        if (position == 0){
-            return "이름"
-        } else if (position == 1){
-            return "폰번"
-        }else{
-            return "거주지"
+
+        return when(position){
+            0 -> "이름"
+            1 -> "폰번"
+            else -> "거주지"
         }
+
     }
 
     override fun getItem(position: Int): Fragment {
 
-        if (position == 0){
-            return NameFragment()
-        } else if (position == 1) {
-            return PhoneFragment()
-        } else {
-            return AddressFragment()
+        return when(position){
+            0 -> NameFragment()
+            1 -> PhoneFragment()
+            else -> AddressFragment()
         }
 
     }
