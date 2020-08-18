45fc3a43e428933e8f772ce683438b0ebd762a2a
이용찬 <lee9890@naver.com>
2020-08-18 20:38:16 +0900

    필요한 프래그먼트 3개추가작업

---
+++ app/src/main/java/kr/co/tjoeun/viewpager_20200818/fragments/PhoneFragment.kt
@@ -0,0 +1,18 @@
+package kr.co.tjoeun.viewpager_20200818.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kr.co.tjoeun.viewpager_20200818.R
+
+class phoneFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_phone, container, false)
+    }
+}
